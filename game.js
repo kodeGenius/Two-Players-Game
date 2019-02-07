2fd7570183697e74e0abe7b7df2fdb4ce7cac612
Saheed Oladosu <oladosu777@gmail.com>
2019-02-07 13:02:00 +0100

    logis testing

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 function start(){
-	let userInput = document.getElementById('userSelect').value;
-	userInput = userInput.toLowerCase();
-	console.log(`you entered ${userInput}`)
-
+	function user(){
+		let userInput = document.getElementById('userSelect').value;
+		if (userInput == 'paper'||userInput=='rock'||userInput=='scissors'){
+			return userInput;
+		}else{return "Enter a valid option";}
+	}
 	
 	function comInput(){
 		let rand = Math.floor(Math.random()*3);
@@ -14,16 +16,17 @@ function start(){
 			return"paper";
 		}
 	} 
+	let userInput = user();
 	let comp = comInput();
+	console.log(`you entered ${userInput}`)
 	console.log(`computer enters ${comp}`)
 
 	function compare(){
-		
 		if(userInput === 'paper'){
 		    if(comp === 'rock'){
 		      return 'You Won!';
 		    }else{
-		      return 'Computer Won';
+		      return 'Computer Won!';
 		    }
 	  	}
 	    if(userInput === 'scissors'){
@@ -41,8 +44,12 @@ function start(){
 		    }
 		}
 	}
+	if(userInput == 'rock' || 'scissors' || 'paper'){
 	if ( userInput == comp){
 			document.getElementById('result').innerHTML = `This is a Tie!`;
 		}else{
 	document.getElementById('result').innerHTML = `${compare()}`}
+	}else{
+		document.getElementById('result').innerHTML = `Please enter a valid option`;
+	}
 }
